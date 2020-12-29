0c9728be0be8538f7ff52ca6fbdf28f07165b698
Zeeyon <zeeyon.c@gmail.com>
2020-12-29 20:50:35 +0900

    refactor: @EnableAutoConfiguration -> @SpringBootApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //당장은 datasource가 필요없기에 추가해놓는다.
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //당장은 datasource가 필요없기에 추가해놓는다.
 public class TestController {
 
     @GetMapping("/test")
