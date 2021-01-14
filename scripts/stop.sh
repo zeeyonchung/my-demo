176309f819b10197848ae4185ed9a2351e7920d7
Zeeyon <zeeyon.c@gmail.com>
2021-01-14 23:56:29 +0900

    fix: typo

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,7 +5,7 @@ CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  ehco ">>> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo ">>> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
